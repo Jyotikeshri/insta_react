e5b5981943fea9a9fc923fd9b6b99f8b28f48676
Jyotikeshri <156110025+Jyotikeshri@users.noreply.github.com>
2024-02-19 02:56:35 +0530

    Add files via upload

---
+++ vite-project/src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,77 @@
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  VStack,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+import Login from "./Login";
+import Signup from "./signupForm";
+import GoogleAuth from "./GoogleAuth";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+
+          {isLogin ? <Login /> : <Signup />}
+
+          {/* {error && (
+            <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+              <AlertIcon fontSize={12} />
+              {error.message}
+            </Alert>
+          )} */}
+
+          {/* ---------------- OR -------------- */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            my={4}
+            gap={1}
+            w={"full"}
+          >
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+            <Text mx={1} color={"white"}>
+              OR
+            </Text>
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+          </Flex>
+
+          <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
+        </VStack>
+      </Box>
+
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.500"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Sign up" : "Log in"}
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default AuthForm;
